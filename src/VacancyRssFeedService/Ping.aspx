ec80a4a767291b4cae20bc6bb4c3b201003acbc5
codewolf86 <chris@codewolfconsulting.com>
2020-03-13 10:30:50 +0000

    Add Ping endpoint to RSS Feed

---
+++ src/VacancyRssFeedService/Ping.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Ping.aspx.cs" Inherits="VacancyRssFeedService.Ping" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Pong</title>
+</head>
+<body>
+    Pong<br />
+    <%= AppVersion %>
+</body>
+</html>
---
+++ src/VacancyRssFeedService/Ping.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+
+namespace VacancyRssFeedService
+{
+    public partial class Ping : System.Web.UI.Page
+    {
+        public string AppVersion { get; set; }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            AppVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+    }
+}
